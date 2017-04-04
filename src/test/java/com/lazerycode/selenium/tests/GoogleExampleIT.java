23c2bd2c2154f556976166a45984a35a78c00038
Hyunil Shin <hyunil.shin@nhnent.com>
2017-04-04 18:09:59 +0900

    modify

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleIT.java
@@ -1,10 +1,14 @@
 package com.lazerycode.selenium.tests;
 
 import com.lazerycode.selenium.DriverBase;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -35,6 +39,7 @@ public class GoogleExampleIT extends DriverBase {
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
 
+        // 실험 #1
         // Google's search is rendered dynamically with JavaScript.
         // Wait for the page to load, timeout after 10 seconds
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
@@ -42,7 +47,24 @@ public class GoogleExampleIT extends DriverBase {
                 return d.getTitle().toLowerCase().startsWith("cheese!");
             }
         });
-
+       
+        // 실험 #2
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.textToBePresentInElementLocated(
+//        	By.cssSelector("#rso > div:nth-child(1) > div > div > div > div > div > span > em"), "Cheese"));
+//
+       
+        // 실험 #3 (실패하는 케이스)
+//        WebElement r = driver.findElement(By.cssSelector("#rso > div:nth-child(1) > div > div > div > div > div > span > em"));
+//        System.out.println("result: " + r.getText());
+//       
+       
+        // 실험 #4
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        WebElement r = driver.findElement(By.cssSelector("#rso > div:nth-child(1) > div > div > div > div > div > span > em"));
+//        System.out.println("result: " + r.getText());
+//
+        
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
     }
